94b1cad52e940dc9ec4e37d6fc9c8eab4b392d9c
AhnJungUn <wjddjs1102@naver.com>
2017-09-07 23:35:03 +0900

    Update and rename fuzz_ole.py to OLE_fuzz.py

---
+++ OLE_fuzz.py
@@ -0,0 +1,38 @@
+from pyZZUF import *
+from random import choice
+import OleFileIO_PL
+import shutil
+import os
+
+
+class OLE_FUZZ:
+
+    def __init__(self, seed_dir, out_dir, filename):
+        self.TARGET = out_dir + filename 
+
+    def Mutation(self):
+
+        with open(self.TARGET, 'rb') as f:
+            ole = f.read()
+
+        ole = ole[8:]
+        ole = pyZZUF(ole)
+        ole.set_ratio(0.3)
+        
+        ole_write = ole.mutate().tostring()
+        try:
+            with open(self.TARGET, 'wb') as f:
+                f.write(ole_write)
+            return True
+        except IOError as error:
+            print error
+            return False
+
+    def emptyTemp():
+        while len(os.listdir("out_dir")) != 0 :
+            for x in os.listdir("out_dir"):
+                try:
+                    os.remove(r"out_dir\%s" % x)
+                except:
+                    pass
+
