10bddbd6d5e7256f4326b9619eefc9580f9ce00e
doravelee <31274946+doravelee@users.noreply.github.com>
2017-09-17 19:08:11 +0900

    ole 구조 가지는 파일 확장자 및 pdf, chm, rtf 파일 확장자 추가

--- OLE_fuzzer.py
+++ OLE_fuzzer.py
@@ -9,7 +9,7 @@ from pyZZUF import *
 from random import choice
 import shutil
 
-class OLE_FUZZ:
+class DOC_FUZZ:
 
     def __init__(self, seed_dir, out_dir, filename):
         self.SEED_DIR = seed_dir
@@ -20,7 +20,7 @@ class OLE_FUZZ:
 
     #------------------------------------------------------------
     # 함수명 : Mutation
-    # 설  명 : OLE 구조를 가지는 파일을 뮤테이션시킨 후 파일에 쓴다.
+    # 설  명 : DOC 파일을 뮤테이션시킨 후 파일에 쓴다.
     #------------------------------------------------------------
     def Mutation(self):
 
@@ -29,12 +29,19 @@ class OLE_FUZZ:
         # 확장자 확인하기
         ext = self.FILENAME.split(".")[1]
         #print "file extension : %s" % ext
-        
-        # 확장자별로 맞춤형 mutation하기
+
+        # ole 구조 가지는 파일 확장자별로 맞춤형 mutation하기
         if ext == "hwp" or ext == "xls":
-            self.new_data = self.fuzz_without_sub_header(data)
+            self.new_data = self.ole_fuzz_without_sub_header(data)
         elif ext == "doc" or ext == "ppt":
-            self.new_data = self.fuzz_with_sub_header(data)
+            self.new_data = self.ole_fuzz_with_sub_header(data)
+        # ole 구조가 아닌 doc 확장자별로 맞춤형 mutation하기
+        elif ext == "pdf":
+            self.new_data = self.pdf_fuzz(data)
+        elif ext == "chm":
+            self.new_data = self.chm_fuzz(data)
+        elif ext == "rtf":
+            self.new_data = self.rtf_fuzz(data)
         else:
             self.new_data = None
         
@@ -44,12 +51,12 @@ class OLE_FUZZ:
             fp.write(self.new_data)
 
     #------------------------------------------------------------
-    # 함수명 : fuzz_without_sub_header
-    # 설  명 : V3에서 sub_header를 검사하지 않는 파일을 뮤테이션시킨다.
+    # 함수명 : ole_fuzz_without_sub_header
+    # 설  명 : V3에서 sub_header를 검사하지 않는 OLE 구조를 가지는 파일을 뮤테이션시킨다.
     # 인자값 : data : 뮤테이션시킬 데이터
     # 반환값 : rdata : 뮤테이션시킨 데이터
     #------------------------------------------------------------
-    def fuzz_without_sub_header(self, data):
+    def ole_fuzz_without_sub_header(self, data):
 
         signature = data[0:8]
 
@@ -60,12 +67,12 @@ class OLE_FUZZ:
         return rdata
 
     #------------------------------------------------------------
-    # 함수명 : fuzz_with_sub_header
-    # 설  명 : V3에서 sub_header를 검사하는 파일을 뮤테이션시킨다.
+    # 함수명 : ole_fuzz_with_sub_header
+    # 설  명 : V3에서 sub_header를 검사하는 OLE 구조 가지는 파일을 뮤테이션시킨다.
     # 인자값 : data : 뮤테이션시킬 데이터
     # 반환값 : rdata : 뮤테이션시킨 데이터
     #------------------------------------------------------------
-    def fuzz_with_sub_header(self, data):
+    def ole_fuzz_with_sub_header(self, data):
 
         signature = data[0:8]
 
@@ -78,4 +85,52 @@ class OLE_FUZZ:
         rdata += sub_signature
         rdata += pyZZUF(data[516:]).mutate().tostring()
 
+        return rdata
+
+    #------------------------------------------------------------
+    # 함수명 : pdf_fuzz
+    # 설  명 : pdf 파일을 뮤테이션시킨다.
+    # 인자값 : data : 뮤테이션시킬 데이터
+    # 반환값 : rdata : 뮤테이션시킨 데이터
+    #------------------------------------------------------------
+    def pdf_fuzz(self, data):
+
+        signature = data[0:4]
+
+        rdata = ""
+        rdata += signature
+        rdata += pyZZUF(data[4:]).mutate().tostring()
+        
+        return rdata
+
+    #------------------------------------------------------------
+    # 함수명 : chm_fuzz
+    # 설  명 : chm 파일을 뮤테이션시킨다.
+    # 인자값 : data : 뮤테이션시킬 데이터
+    # 반환값 : rdata : 뮤테이션시킨 데이터
+    #------------------------------------------------------------
+    def chm_fuzz(self, data):
+
+        signature = data[0:4]
+
+        rdata = ""
+        rdata += signature
+        rdata += pyZZUF(data[4:]).mutate().tostring()
+        
+        return rdata
+
+    #------------------------------------------------------------
+    # 함수명 : rtf_fuzz
+    # 설  명 : rtf 파일을 뮤테이션시킨다.
+    # 인자값 : data : 뮤테이션시킬 데이터
+    # 반환값 : rdata : 뮤테이션시킨 데이터
+    #------------------------------------------------------------
+    def rtf_fuzz(self, data):
+
+        signature = data[0:6]
+
+        rdata = ""
+        rdata += signature
+        rdata += pyZZUF(data[6:]).mutate().tostring()
+        
         return rdata
