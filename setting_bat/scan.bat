b1ff17590e6c4cb59777c1246d6a85a9f182e2e8
bearics <bearics@gmail.com>
2017-09-30 02:34:34 +0900

    Update main.py to no debugger

--- setting_bat/scan.bat
+++ setting_bat/scan.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\Program Files\AhnLab\V3Lite30\V3LMedic.exe" /manual_scan /target:C:\fuzz\in1
+"C:\Program Files\AhnLab\V3Lite30\V3LMedic.exe" /manual_scan /target:C:\fuzz\temp\
 pause
