696e6ced4a73117c410c3bb8795f26f69921b017
bearics <bearics@gmail.com>
2017-11-11 22:05:10 +0900

    add bat

--- setting_bat/git-setting.bat
+++ setting_bat/git-setting.bat
@@ -4,4 +4,6 @@ cd C:\fuzz\fuzzer
 rmdir /s /q C:\fuzz\fuzzer\av_ioctlFuzzer
 git clone https://github.com/pwn2expoit/av_ioctlFuzzer/
 copy C:\fuzz\fuzzer\av_ioctlFuzzer\* C:\fuzz\fuzzer\*
+copy C:\fuzz\fuzzer\av_ioctlFuzzer\setting_bat\Start_fuzzer.bat %USERPROFILE%\Start_fuzzer.bat
+copy C:\fuzz\fuzzer\av_ioctlFuzzer\setting_bat\start_FFV3.bat %USERPROFILE%\start_FFV3.bat
 pause
---
+++ setting_bat/start_FFV3.bat
@@ -0,0 +1,3 @@
+@echo on
+python C:\fuzz\fuzzer\v3_ff.py
+pause
